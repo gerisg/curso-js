371fb72d8b4c342b6e3fcc956891c4f8b000be5e
Gerardo Gallardo <geris.g@gmail.com>
2020-06-21 22:54:31 -0300

    Refactor JSON management

---
+++ m2-intro/c4-app-tareas/jsonTasks.js
@@ -0,0 +1,20 @@
+const fs = require('fs');
+const configuration = require('./configuration');
+
+// Read tasks from JSON
+function readTasks() {
+    let filename = configuration.read().filename;
+    const jsonTasks = fs.readFileSync(filename, 'utf-8');
+    return JSON.parse(jsonTasks);
+}
+
+// Save tasks to JSON
+function writeTasks(tasks) {
+    let filename = configuration.read().filename;
+    let jsonTasks = JSON.stringify(tasks, null, ' ');
+    fs.writeFileSync(filename, jsonTasks);
+}
+
+module.exports = {
+    readTasks, writeTasks
+};
