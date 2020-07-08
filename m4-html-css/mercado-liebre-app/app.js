93b145b68474be67af67716e2ffb1099ebc73e68
Gerardo Gallardo <geris.g@gmail.com>
2020-07-07 21:57:18 -0300

    Register and Login added

--- m4-html-css/mercado-liebre-app/app.js
+++ m4-html-css/mercado-liebre-app/app.js
@@ -5,6 +5,10 @@ app.use(express.static('public'));
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/views/index.html');
+}).get('/register', (req, res) => {
+    res.sendFile(__dirname + '/views/register.html');
+}).get('/login', (req, res) => {
+    res.sendFile(__dirname + '/views/login.html')
 });
 
 app.listen(3000, () => console.log('servidor levantado')); 
