ede656899aed220b868ffc51bfccc4cbdb5435b2
Gerardo Gallardo <geris.g@gmail.com>
2020-07-07 23:48:07 -0300

    Save form data into Json files. Small bugs fixed.

--- m4-html-css/mercado-liebre-app/app.js
+++ m4-html-css/mercado-liebre-app/app.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const app = express();
+const fs = require('fs');
 
 app.use(express.static('public'));
+app.use(express.urlencoded());
 
 app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/views/index.html');
+}).post('/', (req, res) => {
+    let data = JSON.stringify(req.body, null, ' ');
+    fs.writeFileSync("data.json", data);
     res.sendFile(__dirname + '/views/index.html');
 }).get('/register', (req, res) => {
     res.sendFile(__dirname + '/views/register.html');
