4fa230325cd7c339333a9b776142f85ce7cfb456
Gerardo Gallardo <geris.g@gmail.com>
2020-07-11 02:09:22 -0300

    Config app from console

--- m2-intro/c4-app-tareas/run.js
+++ m2-intro/c4-app-tareas/run.js
@@ -9,11 +9,22 @@ const CONFIG = {
     }
 };
 
+function buildConf(params) {
+    let conf = CONFIG;
+    if(params && params.length == 4) {
+        conf.filename = __dirname + '/' + params[0]
+        conf.state_colors['pendiente'] = params[1];
+        conf.state_colors['en progreso'] = params[2];
+        conf.state_colors['terminada'] = params[3];
+    }
+    return conf;
+}
+
 // Execution from console
 let action = process.argv[2];
 let params = process.argv.slice(3);
 let response = action == '--save-conf' ? 
-    app.config(CONFIG) : 
+    app.config(buildConf(params)) : 
     app.execute(action, params);
 
-console.log('Exit code ' + response);
+console.log(response.message);
