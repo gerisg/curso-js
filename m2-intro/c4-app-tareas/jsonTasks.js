d30c0503e717c2f306b8b5e4de54c8f7a28e1717
Gerardo Gallardo <geris.g@gmail.com>
2020-07-11 02:08:01 -0300

    Fix config file not exist

--- m2-intro/c4-app-tareas/jsonTasks.js
+++ m2-intro/c4-app-tareas/jsonTasks.js
@@ -4,6 +4,9 @@ const configuration = require('./configuration');
 // Read tasks from JSON
 function readTasks() {
     let filename = configuration.read().filename;
+    if (!fs.existsSync(filename)) {
+        return [];
+    }
     const jsonTasks = fs.readFileSync(filename, 'utf-8');
     return JSON.parse(jsonTasks);
 }
