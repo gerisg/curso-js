abfdd756ac7e159c6cad038ed5e879546cd581c3
Gerardo Gallardo <geris.g@gmail.com>
2020-08-15 01:26:00 -0300

    Maintenance mode. Error 503. Error 404.

--- m5-express/grupos-json-crud/app.js
+++ m5-express/grupos-json-crud/app.js
@@ -5,6 +5,7 @@ const methodOverride = require('method-override');
 const mainRoutes = require('./routes/index');
 const groupsRoutes = require('./routes/groups');
 const usersRoutes = require('./routes/users');
+const maintenance = require('./middleware/maintenance');
 
 // Configuración
 // Las vistas tienen extension .ejs
@@ -13,10 +14,10 @@ app.set('view engine', 'ejs');
 const maintenanceMode = false;
 
 // Middlewares
-app.use((req,res,next) => maintenanceMode ? res.render('maintenance-mode') : next()); // Maintenance mode
 app.use(express.static('public')); // Template Engines
 app.use(express.urlencoded({ extended: false })); // Forms
 app.use(methodOverride('_method')); // Support PUT and DELETE Methods
+if(maintenanceMode) { app.use(maintenance) } // Maintenance mode
 
 // Routes
 app.use('/', mainRoutes);
@@ -25,7 +26,7 @@ app.use('/users', usersRoutes);
 
 // Errors
 app.use((req, res, next) => {
-    res.status(404).render('404-page');
+    res.status(404).render('404');
     next();
 });
 
